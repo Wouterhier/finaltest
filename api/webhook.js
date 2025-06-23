e0a7036796765f16c3ff998c86ea31c2042fdca7
Wouterhier <wouter@slettenhaar.com>
2025-06-23 19:21:14 +0800

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,20 +1,3 @@
 export default function handler(req, res) {
-  if (req.method === 'GET') {
-    const VERIFY_TOKEN = "test123";
-    const mode = req.query['hub.mode'];
-    const token = req.query['hub.verify_token'];
-    const challenge = req.query['hub.challenge'];
-
-    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-      res.status(200).send(challenge);
-    } else {
-      res.sendStatus(403);
-    }
-  } else if (req.method === 'POST') {
-    console.log("Webhook POST: ", JSON.stringify(req.body));
-    res.status(200).send('EVENT_RECEIVED');
-  } else {
-    res.setHeader('Allow', ['GET', 'POST']);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
-  }
+  res.status(200).send("Webhook endpoint is live.");
 }
